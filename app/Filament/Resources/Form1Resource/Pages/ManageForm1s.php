98048927d6852ae6689462da89b2b3ed3a988ae6
aryadilas <aryadilas@gmail.com>
2025-05-03 07:07:44 +0700

    Add import in form 1

--- app/Filament/Resources/Form1Resource/Pages/ManageForm1s.php
+++ app/Filament/Resources/Form1Resource/Pages/ManageForm1s.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Form1Resource\Pages;
 use App\Filament\Resources\Form1Resource;
 use Filament\Actions;
 use Filament\Resources\Pages\ManageRecords;
+use Konnco\FilamentImport\Actions\ImportField;
+use Konnco\FilamentImport\Actions\ImportAction;
 
 class ManageForm1s extends ManageRecords
 {
@@ -34,6 +36,55 @@ class ManageForm1s extends ManageRecords
             
                     return $data;
                 
+                }),
+            ImportAction::make()
+                ->visible(auth()->user()->hasRole('Puskesmas'))
+                ->fields([
+                    // ImportField::make('kode_fasyankes')
+                    //     ->required(),
+                    ImportField::make('nama_kelurahan')
+                        ->required(),
+                    ImportField::make('tahun')
+                        ->required(),
+                    ImportField::make('status_klb')
+                        ->required(),
+                    ImportField::make('mobilitas_penduduk_klb')
+                        ->required(),
+                    ImportField::make('tahun_arsip_tersedia_cr1')
+                        ->required(),
+                    ImportField::make('cakupan_imunisasi_kumulatif_cr1')
+                        ->required(),
+                    ImportField::make('tahun_arsip_tersedia_cr2')
+                        ->required(),
+                    ImportField::make('cakupan_imunisasi_kumulatif_cr2')
+                        ->required(),
+                    ImportField::make('tahun_arsip_tersedia_cr_bias')
+                        ->required(),
+                    ImportField::make('cakupan_imunisasi_kumulatif_cr_bias')
+                        ->required(),
+                    ImportField::make('tahun_arsip_tersedia_cr_tambahan')
+                        ->required(),
+                    ImportField::make('cakupan_imunisasi_kumulatif_cr_tambahan')
+                        ->required(),
+                ])
+                ->handleRecordCreation(function(array $data) { 
+
+                    return static::getModel()::create([
+                        'kode_fasyankes' => auth()->user()->kode_fasyankes,
+                        'village_name' => $data['nama_kelurahan'],
+                        'year' => $data['tahun'],
+                        'q1' => $data['status_klb'],
+                        'q2' => $data['mobilitas_penduduk_klb'],
+                        'q3a' => $data['tahun_arsip_tersedia_cr1'],
+                        'q3b' => $data['cakupan_imunisasi_kumulatif_cr1'],
+                        'q4a' => $data['tahun_arsip_tersedia_cr2'],
+                        'q4b' => $data['cakupan_imunisasi_kumulatif_cr2'],
+                        'q5a' => $data['tahun_arsip_tersedia_cr_bias'],
+                        'q5b' => $data['cakupan_imunisasi_kumulatif_cr_bias'],
+                        'q6a' => $data['tahun_arsip_tersedia_cr_tambahan'],
+                        'q6b' => $data['cakupan_imunisasi_kumulatif_cr_tambahan'],
+                    ]);
+
                 }),
         ];
     }
