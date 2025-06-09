5edda4d1b0e34ccb05f58f28cfe22ebe9b4995fa
aryadilas <aryadilas@gmail.com>
2025-06-09 11:42:55 +0700

    Fix dashboard design

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,7 +1,7 @@
-<div class="flex flex-col justify-around h-full fi-dashboard-page">
+<div class="flex flex-col h-full gap-5 fi-dashboard-page">
     
-    <h1 class="text-2xl sm:text-[40px] font-extrabold text-justify mt-8 max-w-6xl mx-auto" style="margin-top:1cm;"   >Selamat Datang di Dashboard Sistem Informasi ORI KLB Campak-Rubela!</h1>
-    <p class="text-sm sm:text-[20px] font-normal max-w-6xl leading-8 mx-auto text-justify" style="margin-top:1cm;">
+    <h1 class="text-lg sm:text-[30px] font-extrabold text-justify mt-8 max-w-6xl mx-auto" style="margin-top:1cm;">Selamat Datang di Dashboard Sistem Informasi ORI KLB Campak-Rubela!</h1>
+    <p class="text-sm sm:text-[18px] font-normal max-w-6xl mx-auto text-justify mt-10">
         Sistem ini dirancang untuk membantu Anda dalam proses Respon cepat ORI KLB Campak-Rubela  dengan mudah, cepat, dan akurat.
         <br>
         Terima kasih atas dedikasi Anda, dan mari kita bersama-sama menjaga kesehatan masyarakat kita!
