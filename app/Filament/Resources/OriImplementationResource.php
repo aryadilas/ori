413b1ee5e0c803874cc750669fcc1c9ffa158bee
aryadilas <aryadilas@gmail.com>
2025-06-10 11:20:35 +0700

    Fix Error

--- app/Filament/Resources/OriImplementationResource.php
+++ app/Filament/Resources/OriImplementationResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\OriImplementationResource\Pages;
 use App\Filament\Resources\OriImplementationResource\RelationManagers;
 use App\Models\OriImplementation;
+use App\Models\Fasyankes;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
